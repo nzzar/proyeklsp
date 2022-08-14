0aa2080ea6fee85b6299c71ee64671b74327ce3e
Aay18 <Ayunurul.khairunnisa@gmail.com>
2022-08-14 21:43:56 +0700

    nyalain seeder asesor dan event

--- database/seeders/AssesorSeeder.php
+++ database/seeders/AssesorSeeder.php
@@ -23,18 +23,18 @@ class AssesorSeeder extends Seeder
         
         Asesor::insert([
             'user_id' => $user->id,
-            // 'nik' => '12343545',
-            // 'name' => 'Aay bae',
-            // 'birth_date' => '1999-05-20',
-            // 'phone' => '0871232323',
-            // 'gender' => 'p',
-            // 'address' => 'not empty ok !',
-            // 'reg_number' => 'REG-12345678',
-            // 'blanko_number' => 'BLANKO-12345',
-            // 'education' => 'S1 Management',
-            // 'profession' => 'Profession Copywriter',
-            // 'start_date' => '2020-05-20',
-            // 'expired_date' => '2023-05-20',
+            'nik' => '12343545',
+            'name' => 'Aay bae',
+            'birth_date' => '1999-05-20',
+            'phone' => '0871232323',
+            'gender' => 'p',
+            'address' => 'not empty ok !',
+            'reg_number' => 'REG-12345678',
+            'blanko_number' => 'BLANKO-12345',
+            'education' => 'S1 Management',
+            'profession' => 'Profession Copywriter',
+            'start_date' => '2020-05-20',
+            'expired_date' => '2023-05-20',
         ]);
 
     }
